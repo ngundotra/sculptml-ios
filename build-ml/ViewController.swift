0c07104a8b5e6c4c22d21d657584c3f4bd9ff5f5
Noah Gundotra <ngundotra@noahs-macbook-pro.local>
2018-06-25 14:02:54 -0700

    Get rid of specific LayerTableViewControlling.

--- build-ml/ViewController.swift
+++ build-ml/ViewController.swift
@@ -8,34 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
 
     // Example layers to fill out the table
     var layersAvailable = ["Input", "Dense", "Conv2D", "LSTM", "UpSample2D"]
     var layersInfo = ["Specifies input to models", "Simplest deep transform", "Transform that learns spatial relations",
                       "Transform that learns sequential relations", "Replicates data to make image 2x larger"]
     
+    var layerViewDelegate: LayerTableViewController?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        layerViewDelegate = LayerTableViewController(names: layersAvailable)
+        tableView
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // I don't know what this does yet, but it's a part of the UITableViewDataSource protocol
-        return layersAvailable.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UITableViewCell
-        
-        cell.textLabel?.text = layersAvailable[indexPath.row]
-        
-        return cell
     }
 
 
