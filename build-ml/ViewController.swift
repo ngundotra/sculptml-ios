d29436c76abb598eb43d56b440db4e2adc3a64d3
Noah Gundotra <ngundotra@noahs-macbook-pro.local>
2018-06-25 13:11:48 -0700

    Add basic tableView functionality. Working on building UI first, then adding model

--- build-ml/ViewController.swift
+++ build-ml/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // Example layers to fill out the table
+    var layersAvailable = ["Input", "Dense", "Conv2D", "LSTM", "UpSample2D"]
+    var layersInfo = ["Specifies input to models", "Simplest deep transform", "Transform that learns spatial relations",
+                      "Transform that learns sequential relations", "Replicates data to make image 2x larger"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // I don't know what this does yet, but it's a part of the UITableViewDataSource protocol
+        return layersAvailable.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = layersAvailable[indexPath.row]
+        
+        return cell
     }
 
 
