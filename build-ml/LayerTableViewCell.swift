b8d7a0ad0b434fce2dca8523986ff0d9a19033d7
Noah Gundotra <noah@gundotra.org>
2018-06-26 17:39:06 -0700

    add better constraints

--- build-ml/LayerTableViewCell.swift
+++ build-ml/LayerTableViewCell.swift
@@ -11,60 +11,39 @@ import SnapKit
 class LayerTableViewCell: UITableViewCell {
     // This is the programmatic design of the UI for layer cells...
     
-//    let imgUser = UIImageView()
-//    let labUserName = UILabel()
-//    let labMessage = UILabel()
-//    let labTime = UILabel()
-//    var viewsDict = [String: UIView]()
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//
-//        imgUser.backgroundColor = UIColor.blue
-//
-//        imgUser.translatesAutoresizingMaskIntoConstraints = false
-//        labUserName.translatesAutoresizingMaskIntoConstraints = false
-//        labMessage.translatesAutoresizingMaskIntoConstraints = false
-//        labTime.translatesAutoresizingMaskIntoConstraints = false
-//
-//        contentView.addSubview(imgUser)
-//        contentView.addSubview(labUserName)
-//        contentView.addSubview(labMessage)
-//        contentView.addSubview(labTime)
-//
-//        viewsDict = [
-//            "image" : imgUser,
-//            "username" : labUserName,
-//            "message" : labMessage,
-//            "labTime" : labTime,
-//            ]
-//
-//        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"V:|-[image(10)]", options: [], metrics: nil, views: viewsDict))
-//        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"V:[labTime]-|", options: [], metrics: nil, views: viewsDict))
-//        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"V:|-[username]-[message]-|", options: [], metrics: nil, views: viewsDict))
-//        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"H:|-[username]-[image(10)]-|", options: [], metrics: nil, views: viewsDict))
-//        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat:"H:|-[message]-[labTime]-|", options: [], metrics: nil, views: viewsDict))
-//    }
-    
     let layerImg = UIImageView()
     let layerName = UILabel()
+    let layerDesc = UILabel()
+    
     // FIXME: add layer description, maybe button to detail page
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        let layerDescSize: CGFloat = 12.5
         
         contentView.addSubview(layerName)
         contentView.addSubview(layerImg)
+        contentView.addSubview(layerDesc)
         
-//        self.snp.makeConstraints { (make) -> Void in
-//            make.height.equalTo(70)
-//        }
+        // Layer Name constraints
         layerName.snp.makeConstraints { (make) -> Void in
             make.top.equalToSuperview().offset(20)
             make.left.equalToSuperview().offset(10)
-            
         }
+        // make layer name bold
+        layerName.font = UIFont.boldSystemFont(ofSize: layerName.font.pointSize)
         
+        // Layer Description constraints
+        layerDesc.snp.makeConstraints{ (make) -> Void in
+            make.top.equalTo(layerName.snp.bottom).offset(5)
+            make.bottom.equalToSuperview().offset(-5)
+            make.left.equalToSuperview().offset(20)
+        }
+        // set layer description font size
+        layerDesc.font = UIFont(name: layerDesc.font.fontName, size: layerDescSize)
+        
+        
+        // Layer Image constraints
         layerImg.snp.makeConstraints{(make) -> Void in
             make.top.equalToSuperview().offset(5)
             make.right.equalToSuperview().offset(-10)
