cb1c3144907929df228a2f9741c63bbb6b93898e
Alex Chan <alex.chan@berkeley.edu>
2018-10-24 13:04:13 -0700

    Autolayout menu

--- build-ml/AppDelegate.swift
+++ build-ml/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-eclass AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
